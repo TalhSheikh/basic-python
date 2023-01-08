d14c2b3bf93827679a85db261320fb3f5f09d9c1
Talha Sheikh <121301354+TalhSheikh@users.noreply.github.com>
2023-01-08 15:30:11 +0500

    Add files via upload

---
+++ Python for Beginners/index.py
@@ -0,0 +1,131 @@
+# These are the Basics of Python
+
+# print("Hello World")
+# myvar = 2+2/2
+# print(myvar)
+
+# myvar1 = int(input())
+# myvar2 = int(input())
+# sum = myvar1 + myvar2
+# print(sum)
+
+# if 5>90:
+#     print("false")
+# elif 90>=100:
+#     print("true")
+# else:
+#     print("You Can't Pass")
+
+# for num in range(12):
+#     print("Hi")
+
+# num = 0
+# while (num<100):
+#     print("Thanks")
+#     num = num+1
+
+# def talha():
+#     print("Hello World")
+# talha()
+
+# def add(num1,num2):
+#     sum = num1 + num2
+#     print(sum)
+#     return
+# add(1,6)
+# add(6,6)
+
+# import math
+# math.pow(4,4)
+
+#used to jst write INTEGER number not float
+
+# import math
+# a = math.floor(4.3)
+# b = math.floor(6.3)
+# print(a)
+# print(b)
+
+#used to round off
+
+# import math
+# a = math.ceil(4.5)
+# b = math.ceil(6.5)
+# print(a)
+# print(b)
+
+#abs() show the absolute value of the value passed
+
+# a = abs(10)
+# b = abs(-10)
+# print(a)
+# print(b)
+
+#log of the given value
+
+# import math
+# a = math.log(10)
+# b = math.log(10,2)
+# print(a)
+# print(b)
+
+#use to get square root of the given number
+
+# import math 
+# a = math.sqrt(9)
+# b = math.sqrt(16)
+# print(a)
+# print(b)
+
+#this may not work on VSC try it on CMD Python
+
+# str = "karachi"
+# str.capitalize()
+# str.count("a")
+# a = str.find("k")
+# print(a)
+
+# this will work on VSC
+
+# str = " "
+# joining= ("I","am","great")
+# a = str.join(joining)
+# print(a)
+# str = "-"
+# joining= ("I","am","great")
+# a = str.join(joining)
+# print(a)
+
+# list or Array
+# lis1 = ["1", 1]
+# lis1.append(10)
+# del lis1[1]
+# print(lis1)
+
+# Accessing data from tuple it is same as LIST, but can not update or delete like we do in list
+# tuple1 = ["1", 1]
+# print(tuple1[0])
+
+# dictionary or an object
+# dict1 = {
+#     'name': 'talha',
+#     'age': 16,
+#     'hobby':'Learning New Things'
+# }
+# print(dict1['name'])
+
+# updating dictionary
+# dict1['name']="Talha Sheikh"
+# print(dict1['name'])
+
+# deleting data in dictionary
+# del dict1['hobby']
+# print(dict1['name'])
+# del dict1 #this will deletee the whole dictionary
+#use the 'clear()' function to empty the dictionary
+
+# i = sum = 0
+# while i<= 4:
+#     sum += i
+#     i = i+1
+#     print(sum)
